34d0546fa7995f5b68add7e0d930b07f38555715
Nicodab <nicolas.fischer78@gmail.com>
2021-10-26 01:05:57 +0200

    Alternate message authors in channel

--- labs/lab4/src/Components/Messages.js
+++ labs/lab4/src/Components/Messages.js
@@ -6,8 +6,19 @@ import React from 'react'
 import {useState} from 'react';
 import { SendBox } from './SendBox';
 
+var counter = 0
+
 export const Messages = () => {
     const addMessage = (message) => {
+      // Alternate message between users
+        if(counter%2 == 0){
+          message.author = 'Sergei'
+          counter++
+        }
+        else{
+          message.author = 'David'
+          counter++
+        }
         setMessages([
           ...messages,
           message
@@ -34,26 +45,27 @@ export const Messages = () => {
     
     return (
         <div className='messages'>
-<nav>
-<ul className='list'>
-              { messages.map( (message, i) => (
-                <li key={i} className='li'>
-                  <p>
-                    <span>{message.author}</span>
-                    {' '}
-                    <span>{(new Date(message.creation)).toString()}</span>
-                  </p>
-                  <div>
-                    {
-                      message.content
-                      .split(/(\n +\n)/)
-                      .filter( el => el.trim() )
-                      .map( el => <p>{el}</p>)
-                    }
-                  </div>
-                </li>
-              ))}
-            </ul>
+            <nav>
+              <ul className='list'>
+                { messages.map( (message, i) => (
+                    <li key={i} className='li'>
+                      <p>
+                        <span>{message.author}</span>
+                        {' '}
+                        <span>{(new Date(message.creation)).toString()}</span>
+                      </p>
+                      <div>
+                        {
+                          message.content
+                          .split(/(\n +\n)/)
+                          .filter( el => el.trim() )
+                          .map( el => <p>{el}</p>)
+                        }
+                      </div>
+                    </li>
+                  ))
+                }
+              </ul>
             </nav>
 
             <div className='form'>
