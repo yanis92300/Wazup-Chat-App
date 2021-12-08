5e0116f322397fc98d77122f421b1cf79611c55e
yanis92300 <57595838+yanis92300@users.noreply.github.com>
2021-12-08 01:47:42 +0100

    front_end

--- labs/lab2/lib/index.js
+++ labs/lab2/lib/index.js
@@ -1,55 +1,55 @@
- const express = require('express')
-const app = express()
-const ejs = require('ejs')
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
 const config = {
-    port: 3000
-  }
-  const data = {
-    channels: [{
-      id: '1',
-      name: 'Channel 1',
-    },{
-      id: '2',
-      name: 'Channel 2',
-    },{
-      id: '3',
-      name: 'Channel 3',
-    }]
-  }
-  
-  app.get('/', (req, res) => {
-    //res.send(' <h1>Home page </h1><body><a href ="/channels/">See Channels </a></body>') 
-  
-  })
+  port: 3000,
+};
+const data = {
+  channels: [
+    {
+      id: "1",
+      name: "Channel 1",
+    },
+    {
+      id: "2",
+      name: "Channel 2",
+    },
+    {
+      id: "3",
+      name: "Channel 3",
+    },
+  ],
+};
 
+app.get("/", (req, res) => {
+  res.send(
+    ' <h1>Home page </h1><body><a href ="/channels/">See Channels </a></body>'
+  );
+});
 
-
-
-  app.get("/channels", (req,res) => {
-
-    res.send(
-      data.channels.map(product =>
-        `<body>
+app.get("/channels", (req, res) => {
+  res.send(
+    data.channels
+      .map(
+        (product) =>
+          `<body>
         <ul>
         <li><a href ="/channels/${product.id}">${product.id}</a> </li>
          </ul>
         </body>
         `
-      ).join("")
-    )
-  })
+      )
+      .join("")
+  );
+});
 
-  
-  app.get('/channels/:id', (req, res) => {
-   
-    const { id } = req.params
-    const channel = data.channels.find(e => e.id === req.params.id)
+app.get("/channels/:id", (req, res) => {
+  const { id } = req.params;
+  const channel = data.channels.find((e) => e.id === req.params.id);
 
+  res.send(channel.name);
+});
 
-    res.send( channel.name
-    )
-  })
-  
-  app.listen(config.port, () => {
-   console.log(`Chat is waiting for you at http://localhost:${config.port}`)
-   })
+app.listen(config.port, () => {
+  console.log(`Chat is waiting for you at http://localhost:${config.port}`);
+});
---
+++ projet/login_page/login/src/App.js
@@ -0,0 +1,40 @@
+import { Button, makeStyles } from "@material-ui/core";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
+import * as React from "react";
+import { Navbar } from "./components/Navbar";
+import { Leftbar } from "./components/Leftbar";
+import { Grid } from "@mui/material";
+import { Messages } from "./components/Messages";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: "100vh",
+  },
+}));
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Navbar />
+      <Grid container className={classes.container}>
+        <Grid item xs={2}>
+          <Leftbar />
+        </Grid>
+        <Grid
+          item
+          xs={10}
+          style={{
+            backgroundColor: "white",
+            borderLeft: "solid 1px",
+            borderLeftColor: "#dbd9d9",
+          }}
+        >
+          <Messages />
+        </Grid>
+        {/* <Grid item sm={3}></Grid> */}
+      </Grid>
+    </div>
+  );
+};
+
+export default App;
