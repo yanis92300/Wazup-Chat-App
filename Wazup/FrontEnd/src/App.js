32dfa48a892c31fab799bf6dc1ce55f5d38b8863
yanis92300 <57595838+yanis92300@users.noreply.github.com>
2021-12-15 18:19:47 +0100

    login/Signup

--- Wazup/FrontEnd/src/App.js
+++ Wazup/FrontEnd/src/App.js
@@ -6,63 +6,60 @@ import { Leftbar } from "./components/Leftbar";
 import { Grid } from "@mui/material";
 import { Messages } from "./components/Messages";
 import { Main } from "./components/Main";
-import {
-  Route,
-  Routes,
-  Navigate,
-  useLocation
-} from "react-router-dom"
-
-
+import Login from "./components/Login";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import Signup from "./components/SignUp";
 const App = () => {
   const location = useLocation();
 
-  const gochannels = (<Navigate
-    to={{
-      pathname: "/channels",
-      state: { from: location }
-    }}
-  />)
-  const gohome = (<Navigate
-    to={{
-      pathname: "/",
-      state: { from: location }
-    }}
-  />)
+  const gochannels = (
+    <Navigate
+      to={{
+        pathname: "/channels",
+        state: { from: location },
+      }}
+    />
+  );
+  const gohome = (
+    <Navigate
+      to={{
+        pathname: "/",
+        state: { from: location },
+      }}
+    />
+  );
   return (
     <div className="App">
-      <Navbar/>
       <Routes>
         {/* <Route exact path="/" element={oauth ? (gochannels) : (<Login />)}/> */}
-        <Route exact path="/" element={(gochannels)}/>
+        <Route exact path="/" element={<Login />} />
         {/* <Route path="/channels/*" element={oauth ? (<Main />) : (gohome)}/> */}
-        <Route path="/channels/*" element={<Main />}/>
+        <Route path="/channels/*" element={<Main />} />
+
+        <Route path="/Signup/*" element={<Signup />} />
       </Routes>
     </div>
 
-
-
     // <div>
-      // <Navbar />
-      // <Grid container className={classes.container}>
-      //   <Grid item xs={2}>
-      //     <Leftbar />
-      //   </Grid>
-      //   <Grid
-      //     item
-      //     xs={10}
-      //     style={{
-      //       backgroundColor: "white",
-      //       borderLeft: "solid 1px",
-      //       borderLeftColor: "#dbd9d9",
-      //     }}
-      //   >
-      //     <Messages />
-      //   </Grid>
-      //   {/* <Grid item sm={3}></Grid> */}
-      // </Grid>
+    // <Navbar />
+    // <Grid container className={classes.container}>
+    //   <Grid item xs={2}>
+    //     <Leftbar />
+    //   </Grid>
+    //   <Grid
+    //     item
+    //     xs={10}
+    //     style={{
+    //       backgroundColor: "white",
+    //       borderLeft: "solid 1px",
+    //       borderLeftColor: "#dbd9d9",
+    //     }}
+    //   >
+    //     <Messages />
+    //   </Grid>
+    //   {/* <Grid item sm={3}></Grid> */}
+    // </Grid>
     // </div>
-    
   );
 };
 
